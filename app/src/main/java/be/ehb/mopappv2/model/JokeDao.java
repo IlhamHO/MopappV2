7b47f667e3667783ee8ba1bf2e73a12d7aff10b5
mobapp10 <mobapp10@student.ehb.be>
2019-02-26 12:27:13 +0100

    Initial commit

---
+++ app/src/main/java/be/ehb/mopappv2/model/JokeDao.java
@@ -0,0 +1,23 @@
+package be.ehb.mopappv2.model;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface JokeDao {
+    @Insert
+    public void insertJoke(Joke nJoke);
+
+    @Delete
+    void deleteJoke(Joke nJoke);
+
+    @Query("SELECT * FROM Joke")
+    List<Joke> selectAllJokes();
+
+    @Query("SELECT * FROM Joke WHERE id = :id")
+    Joke selectJokeById(long id);
+}
